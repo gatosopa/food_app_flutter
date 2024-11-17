27bd07331d0451c2e166422b36b340b7b80e3def
gatosopa <strawberrym01n@gmail.com>
2024-11-17 20:24:22 +0900

    Editable activity remade; fixed typos

--- lib/src/features/camera/presentation/view/editable_page.dart
+++ lib/src/features/camera/presentation/view/editable_page.dart
@@ -14,6 +14,7 @@ class EditablePage extends StatefulWidget {
 }
 
 class _EditablePageState extends State<EditablePage> {
+  final String _userId = "0DFcyMyG9UMkXAgEkKkJubJUo2B3"; // Hardcoded userId for testing
   List<TextEditingController> _controllers = [];
   final Dio _dio = Dio();
 
@@ -23,7 +24,8 @@ class _EditablePageState extends State<EditablePage> {
     // Parse the JSON and initialize controllers for each ingredient
     final data = jsonDecode(widget.jsonData);
     List<String> ingredients = List<String>.from(data['ingredients']);
-    _controllers = ingredients.map((ingredient) => TextEditingController(text: ingredient)).toList();
+    _controllers =
+        ingredients.map((ingredient) => TextEditingController(text: ingredient)).toList();
   }
 
   @override
@@ -33,17 +35,19 @@ class _EditablePageState extends State<EditablePage> {
     super.dispose();
   }
 
-  // Convert the edited list back to JSON format
-String getEditedJson() {
-  List<String> updatedIngredients = _controllers.map((controller) {
-    String ingredient = controller.text.trim();
-    return ingredient.replaceAll(RegExp(r'^\d+\.?\s*'), ''); // Remove leading numbers and dots
-  }).toList();
-  
-  final updatedData = {'message': {'ingredients': updatedIngredients}};
-  return jsonEncode(updatedData);
-}
+  // Convert the edited list back to JSON format with userId
+  String getEditedJson() {
+    List<String> updatedIngredients = _controllers.map((controller) {
+      String ingredient = controller.text.trim();
+      return ingredient.replaceAll(RegExp(r'^\d+\.?\s*'), ''); // Remove leading numbers and dots
+    }).toList();
 
+    final updatedData = {
+      'user_id': _userId, // Hardcoded userId for testing
+      'ingredients': updatedIngredients,
+    };
+    return jsonEncode(updatedData);
+  }
 
   // Function to send edited data to the server
   Future<void> sendEditedData() async {
@@ -53,8 +57,11 @@ String getEditedJson() {
 
     final response = await _dio.post(
       '${Constants.serverIP}/submit_json', // Replace with your server endpoint
-      data: {'message': decodedData}, // Packaged in the same JSON format
+      data: decodedData, // Packaged in the updated JSON format
     );
+
+    // Log the full server response
+    print("Server Response: ${response.data}");
 
     if (response.statusCode == 200) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -77,6 +84,7 @@ String getEditedJson() {
       );
     }
   } catch (e) {
+    print("Error sending data: $e"); // Log the error
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text("Error sending data: $e")),
     );
@@ -92,38 +100,135 @@ String getEditedJson() {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Edit Ingredients'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.save),
-            onPressed: _submit, // Trigger the submit function on save
+      backgroundColor: const Color(0xFFF5F5F5), // Slightly gray background
+      body: CustomScrollView(
+        slivers: [
+          // SliverAppBar for the "FOODIE" app bar
+          SliverAppBar(
+            backgroundColor: Constants.primaryColor,
+            expandedHeight: 110,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(),
+              title: const Text(
+                'F O O D I E',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              centerTitle: true,
+            ),
           ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: _controllers.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: TextField(
-              controller: _controllers[index],
-              decoration: InputDecoration(
-                labelText: 'Ingredient ${index + 1}',
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Editable ingredients list
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: _controllers.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        decoration: BoxDecoration(
+                          color: Colors.white, // White rectangle
+                          borderRadius: BorderRadius.circular(16),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              blurRadius: 6,
+                              offset: const Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: ListTile(
+                          title: TextField(
+                            controller: _controllers[index],
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold, // Bold text for ingredient
+                            ),
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              labelText: 'Ingredient #${index + 1}',
+                              labelStyle: const TextStyle(
+                                color: Colors.red, // Red label text
+                                fontWeight: FontWeight.bold,
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                            ),
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.grey),
+                            onPressed: () {
+                              setState(() {
+                                _controllers.removeAt(index);
+                              });
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Ingredient removed.')),
+                              );
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  const Center(
+                    child: Text(
+                      "Any undetected ingredients? Add More!",
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  // Buttons for Add and Submit
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            // Add a new TextEditingController to the list for a new ingredient
+                            _controllers.add(TextEditingController());
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Constants.primaryColor,
+                          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: const Text(
+                          "Add",
+                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: _submit, // Trigger the submit function
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Constants.primaryColor,
+                          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: const Text(
+                          "Find Recipes",
+                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                ],
               ),
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            // Add a new TextEditingController to the list for a new ingredient
-            _controllers.add(TextEditingController());
-          });
-        },
-        child: Icon(Icons.add),
-        tooltip: 'Add Ingredient',
+          ),
+        ],
       ),
     );
   }
